3e274ef30bfe178fb70ee2e5dc8085d5f1fbd34f
Swapnasubham Das <swapnasubham.das@gmail.com>
2026-02-08 01:08:31 +0530

    JS practice

--- Session/08_7th_February/brain.js
+++ Session/08_7th_February/brain.js
@@ -2,3 +2,8 @@ age = 45
 console.log('Age is ' + age) // Age is 25
 var age = 25
 console.log('Age is ' + age) // Age is 25
+
+// var age = 25
+// console.log('Age is ' + age) // Age is 25
+// var age = 45
+// console.log('Age is ' + age) // Age is 45
