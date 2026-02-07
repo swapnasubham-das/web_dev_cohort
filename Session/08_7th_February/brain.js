35de184d78245ee3ae78f29aa3aafaa6b0736aba
Swapnasubham Das <swapnasubham.das@gmail.com>
2026-02-08 01:02:45 +0530

    javascript started

---
+++ Session/08_7th_February/brain.js
@@ -0,0 +1,4 @@
+age = 45
+console.log('Age is ' + age) // Age is 25
+var age = 25
+console.log('Age is ' + age) // Age is 25
---
+++ Session/08_7th_February/script.js
@@ -0,0 +1,6 @@
+var fname = 'Swapnasubham'
+var lname = 'Das'
+
+// JS is a loosely typed language, we can change the type of variable at any point of time
+
+console.log(fname + ' ' + lname) // Swapnasubham Das
